ee943bdefdc9d71749dc26bbfe254947d8d55d0f
Robin Mordasiewicz <robin@mordasiewicz.com>
2020-07-21 22:12:32 -0400

    changing lower thirds script

--- bin/name.sh
+++ bin/name.sh
@@ -2,7 +2,7 @@
 #
 
 NAME="FU BAR"
-TITLE="JOB TITLE"
+TITLE="SOLUTIONS ENGINEER"
 
 OFFSET=100
 LEFTMARGIN=46
@@ -10,11 +10,14 @@ RIGHTMARGIN=60
 
 NAMEOFFSET=$(( ${OFFSET} + ${LEFTMARGIN} ))
 
-NAMEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 0 -kerning .9 -units pixelspercentimeter -font ./NeusaNextCondensedBold.otf -pointsize 48 -annotate 0 "${NAME}" null: 2>&1 | grep 'Metrics:' | grep -Po ";\swidth:\s[0-9]+.?[0-9]*;" | grep -Po "\d+.?\d*"`
-TITLEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 1 -kerning 1.8 -units pixelspercentimeter -font ./NeusaNextCondensed.otf -pointsize 30 -annotate 0 "${TITLE}" null: 2>&1 | grep 'Metrics:' | grep -Po ";\swidth:\s[0-9]+.?[0-9]*;" | grep -Po "\d+.?\d*"`
+NAMEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 0 -kerning .9 -units pixelspercentimeter -font ./NeusaNextCondensedBold.otf -pointsize 48 -annotate 0 "${NAME}" null: 2>&1 | grep 'Metrics:' | grep -o ";\swidth:\s[0-9]\+.\?[0-9]*;" | grep -o "[0-9]\+.\?[0-9]*"`
+TITLEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 1 -kerning 1.8 -units pixelspercentimeter -font ./NeusaNextCondensed.otf -pointsize 30 -annotate 0 "${TITLE}" null: 2>&1 | grep 'Metrics:' | grep -o ";\swidth:\s[0-9]\+.\?[0-9]*;" | grep -o "[0-9]\+.\?[0-9]*"`
 
 NAMEWIDTH=`echo "($NAMEWIDTH+1)/1" | bc`
 TITLEWIDTH=`echo "($TITLEWIDTH+1)/1" | bc`
+
+echo $NAMEWIDTH
+echo $TITLEWIDTH
 
 if [ ${NAMEWIDTH} -gt ${TITLEWIDTH} ]
 then
@@ -25,30 +28,31 @@ fi
 
 WIDTH=$(( ${OFFSET} + ${LEFTMARGIN} + ${TEXTWIDTH} + ${RIGHTMARGIN} ))
 
+#    -adaptive-blur 0x2 \
+    #-fill "#f1faff" \
 convert  -size 1920x1080 \
     -alpha on \
     -channel a \
-    -evaluate multiply 0.80 +channel \
+    -evaluate multiply 0.65 +channel \
     xc:none \
     -density 75 \
     -gravity Northwest \
     -units pixelspercentimeter \
-    -adaptive-blur 0x2 \
-    -fill "#f1faff" \
+    -fill "rgba( 241, 250, 255 , 0.8 )" \
     -draw "rectangle ${WIDTH},980 ${OFFSET},790" \
-    -font "./fonts/NeusaNextCondensedBold.otf" \
     -fill "#46484c" \
     -stroke "#46484c" \
     -strokewidth 0 \
     -kerning .9 \
     -pointsize 48 \
+    -font "../fonts/NeusaNextCondensedBold.otf" \
     -annotate +${NAMEOFFSET}+820 "${NAME}" \
     -fill "#46484c" \
     -stroke "#46484c" \
     -strokewidth 1 \
     -kerning 1.8 \
     -pointsize 30 \
-    -font "./fonts/NeusaNextCondensed.otf" \
+    -font "../fonts/NeusaNextCondensed.otf" \
     -annotate +${NAMEOFFSET}+881 "${TITLE}" \
 PNG32:nametag.png
 
