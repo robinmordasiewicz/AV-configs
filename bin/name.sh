75df4b1a16b0452cf01e30a81e9be1fbbe024193
Robin Mordasiewicz <robin@mordasiewicz.com>
2020-07-22 00:12:47 -0400

    early stages

--- bin/name.sh
+++ bin/name.sh
@@ -10,14 +10,12 @@ RIGHTMARGIN=60
 
 NAMEOFFSET=$(( ${OFFSET} + ${LEFTMARGIN} ))
 
-NAMEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 0 -kerning .9 -units pixelspercentimeter -font ./NeusaNextCondensedBold.otf -pointsize 48 -annotate 0 "${NAME}" null: 2>&1 | grep 'Metrics:' | grep -o ";\swidth:\s[0-9]\+.\?[0-9]*;" | grep -o "[0-9]\+.\?[0-9]*"`
-TITLEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 1 -kerning 1.8 -units pixelspercentimeter -font ./NeusaNextCondensed.otf -pointsize 30 -annotate 0 "${TITLE}" null: 2>&1 | grep 'Metrics:' | grep -o ";\swidth:\s[0-9]\+.\?[0-9]*;" | grep -o "[0-9]\+.\?[0-9]*"`
+NAMEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 0 -kerning .9 -units pixelspercentimeter -font ./fonts/NeusaNextCondensedBold.otf -pointsize 48 -annotate 0 "${NAME}" null: 2>&1 | grep 'Metrics:' | grep -o "width:\s[0-9]\+\.\?[0-9]\?*" | cut -f 2 -d " "`
+
+TITLEWIDTH=`convert -debug annotate  xc: -density 75 -gravity Northwest -strokewidth 1 -kerning 1.8 -units pixelspercentimeter -font ./fonts/NeusaNextCondensed.otf -pointsize 30 -annotate 0 "${TITLE}" null: 2>&1 | grep 'Metrics:' | grep -o "width:\s[0-9]\+\.\?[0-9]\?*" | cut -f 2 -d " "`
 
 NAMEWIDTH=`echo "($NAMEWIDTH+1)/1" | bc`
 TITLEWIDTH=`echo "($TITLEWIDTH+1)/1" | bc`
-
-echo $NAMEWIDTH
-echo $TITLEWIDTH
 
 if [ ${NAMEWIDTH} -gt ${TITLEWIDTH} ]
 then
@@ -29,7 +27,6 @@ fi
 WIDTH=$(( ${OFFSET} + ${LEFTMARGIN} + ${TEXTWIDTH} + ${RIGHTMARGIN} ))
 
 #    -adaptive-blur 0x2 \
-    #-fill "#f1faff" \
 convert  -size 1920x1080 \
     -alpha on \
     -channel a \
@@ -45,14 +42,14 @@ convert  -size 1920x1080 \
     -strokewidth 0 \
     -kerning .9 \
     -pointsize 48 \
-    -font "../fonts/NeusaNextCondensedBold.otf" \
+    -font "./fonts/NeusaNextCondensedBold.otf" \
     -annotate +${NAMEOFFSET}+820 "${NAME}" \
     -fill "#46484c" \
     -stroke "#46484c" \
     -strokewidth 1 \
     -kerning 1.8 \
     -pointsize 30 \
-    -font "../fonts/NeusaNextCondensed.otf" \
+    -font "./fonts/NeusaNextCondensed.otf" \
     -annotate +${NAMEOFFSET}+881 "${TITLE}" \
 PNG32:nametag.png
 
